1e3b47010c4dab51dd04b57f83fccb484ff7495a
jeremie <alcaraz.jeremie@hotmail.fr>
2019-07-23 02:44:16 +0200

    (Test module security)

--- library-consumer/src/main/java/com/library/dao/model/User.java
+++ library-consumer/src/main/java/com/library/dao/model/User.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -21,12 +23,98 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "firstname")
-    private String firstName;
-
-    @Column(name = "lastname")
-    private String lastName;
     private String mail;
-    //private String encrytedPassword;
 
+    private String password;
+
+    @Column(nullable = false)
+    private String username;
+
+    private int active;
+
+    private String roles = "";
+
+    private String permissions = "";
+
+
+    public User(String mail, String password, String username, int active, String roles, String permissions) {
+        this.mail = mail;
+        this.password = password;
+        this.username = username;
+        this.active = 1;
+        this.roles = roles;
+        this.permissions = permissions;
+    }
+
+    protected User(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public String getPermissions() {
+        return permissions;
+    }
+
+    public List<String> getRoleList(){
+        if(this.roles.length() > 0){
+            return Arrays.asList(this.roles.split(","));
+        }
+        return new ArrayList<>();
+    }
+
+    public List<String> getPermissionList(){
+        if(this.permissions.length() > 0){
+            return Arrays.asList(this.permissions.split(","));
+        }
+        return new ArrayList<>();
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public void setPermissions(String permissions) {
+        this.permissions = permissions;
+    }
 }
--- library-consumer/src/main/java/com/library/dao/repository/UserRepository.java
+++ library-consumer/src/main/java/com/library/dao/repository/UserRepository.java
@@ -5,4 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    User findUserByMail(String mail);
+
+    User findUserByUsername(String username);
+
+
 }
